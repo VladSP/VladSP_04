0bc66a8c9201e08b69d66ec497519725ce154510
VladSP <sp.vlad96@gmail.com>
2023-03-08 00:32:57 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let a = +prompt('Enter first number', '');
+let b = +prompt('Enter second number', '');
+let c = +prompt('Enter third number', '');
+let sum = 0;
+sum = (a + b + c) / 3;
+alert(sum);
+console.log(sum);
